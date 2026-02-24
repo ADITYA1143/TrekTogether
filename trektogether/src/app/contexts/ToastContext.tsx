ca6a8475afc1e9aaba7f9e8b1a3bbdaf27728960
Aditya <adityaghumare14@gmail.com>
2026-02-24 22:35:33 +0530

    solved frontend errors

--- trektogether/src/app/contexts/ToastContext.tsx
+++ trektogether/src/app/contexts/ToastContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, ReactNode, useCallback } from 'react';
+import { createContext, useState, useCallback } from 'react';
+import type { ReactNode } from "react";
 
 export type ToastType = 'success' | 'error';
 
