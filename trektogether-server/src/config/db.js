7038f757d37d73ef6f29cc695920e5d9c10f222d
Aditya <adityaghumare14@gmail.com>
2026-04-03 21:30:28 +0530

    fix: update MongoDB connection to resolve DNS issue

--- trektogether-server/src/config/db.js
+++ trektogether-server/src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+  family: 4   // 👈 THIS LINE FIXES RENDER DNS ISSUE
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.log("❌ MongoDB error:", err));
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB error:", error.message);
